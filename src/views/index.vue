4d133b3041acaaccb68fc356f7327faa13d2a39e
xjfizz <58769319@qq.com>
2021-03-09 09:58:00 +0800

    2021-3-9  fix  首页

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,98 +1,71 @@
 <template>
-  <div class="dashboard-editor-container">
 
-    <panel-group @handleSetLineChartData="handleSetLineChartData"/>
-
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-      <line-chart :chart-data="lineChartData"/>
-    </el-row>
-
-    <el-row :gutter="32">
-      <el-col :lg="8" :sm="24" :xs="24">
-        <div class="chart-wrapper">
-          <raddar-chart/>
+  <div class="index-main">
+    <div class="content">
+      <div class="top">
+        <img src="../assets/logo/kylin_logo.png" />
+        <span>巨象Faas共享工场后台管理系统</span>
         </div>
-      </el-col>
-      <el-col :lg="8" :sm="24" :xs="24">
-        <div class="chart-wrapper">
-          <pie-chart/>
-        </div>
-      </el-col>
-      <el-col :lg="8" :sm="24" :xs="24">
-        <div class="chart-wrapper">
-          <bar-chart/>
-        </div>
-      </el-col>
-    </el-row>
-
+      <div class="bottom">Giant Elephant Faas Shared Workshop Backstage Management System</div>
+    </div>
+  </div>
 
   </div>
 </template>
 
 <script>
-import PanelGroup from './dashboard/PanelGroup'
-import LineChart from './dashboard/LineChart'
-import RaddarChart from './dashboard/RaddarChart'
-import PieChart from './dashboard/PieChart'
-import BarChart from './dashboard/BarChart'
 
-const lineChartData = {
-  newVisitis: {
-    expectedData: [100, 120, 161, 134, 105, 160, 165],
-    actualData: [120, 82, 91, 154, 162, 140, 145]
-  },
-  messages: {
-    expectedData: [200, 192, 120, 144, 160, 130, 140],
-    actualData: [180, 160, 151, 106, 145, 150, 130]
-  },
-  purchases: {
-    expectedData: [80, 100, 121, 104, 105, 90, 100],
-    actualData: [120, 90, 100, 138, 142, 130, 130]
-  },
-  shoppings: {
-    expectedData: [130, 140, 141, 142, 145, 150, 160],
-    actualData: [120, 82, 91, 154, 162, 140, 130]
-  }
-}
 
 export default {
   name: 'Index',
   components: {
-    PanelGroup,
-    LineChart,
-    RaddarChart,
-    PieChart,
-    BarChart
+   
   },
   data() {
     return {
-      lineChartData: lineChartData.newVisitis
+     
     }
   },
   methods: {
-    handleSetLineChartData(type) {
-      this.lineChartData = lineChartData[type]
-    }
+   
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.dashboard-editor-container {
-  padding: 32px;
-  background-color: rgb(240, 242, 245);
-  position: relative;
-
-  .chart-wrapper {
-    background: #fff;
-    padding: 16px 16px 0;
-    margin-bottom: 32px;
+  .index-main{
+    width: 100%;
+    height: 80vh;
   }
-}
+  .index-main .content{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height:100vh;
+    background-color: darkgray;
 
-@media (max-width: 1024px) {
-  .chart-wrapper {
-    padding: 8px;
   }
-}
+  .index-main .top{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+    color: black;
+    font-size: 40px;
+    margin: 10px;
+  }
+  .index-main .top img {
+    width: 32px;
+    height: 32px;
+  }
+   .index-main .bottom{
+      display: flex;
+    justify-content: center;
+     align-items: center;
+    font-weight: bold;
+    color: black;
+    font-size: 20px;
+  }
 </style>
