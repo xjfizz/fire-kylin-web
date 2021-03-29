c0d22076c28a37f529964a2e52145a4143caec85
autumn-chendq <autumn-chendq@outlook.com>
2021-03-29 14:39:22 +0800

    Fix refs#202103291439 订单代裁订单列表

---
+++ src/api/garment/proxy/oms/order/order.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询代裁订单列表
+export function listOrder(query) {
+  return request({
+    url: '/oms/web/proxy/order/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询代裁订单详细
+export function getOrder(pkid) {
+  return request({
+    url: '/oms/web/proxy/order/' + pkid,
+    method: 'get'
+  })
+}
+
+// 新增代裁订单
+export function addOrder(data) {
+  return request({
+    url: '/oms/web/proxy/order',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改代裁订单
+export function updateOrder(data) {
+  return request({
+    url: '/oms/web/proxy/order',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除代裁订单
+export function delOrder(pkid) {
+  return request({
+    url: '/oms/web/proxy/order/' + pkid,
+    method: 'delete'
+  })
+}
+
+// 导出代裁订单
+export function exportOrder(query) {
+  return request({
+    url: '/oms/web/proxy/order/export',
+    method: 'get',
+    params: query
+  })
+}
